e8b2699ce17877482f843616d9ea382354bff9ad
Felipe Pinto <felipe14062000@gmail.com>
2023-06-17 14:41:54 -0300

    test: testando o funcionamento da classe dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,22 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 2
 def test_dish():
-    pass
+    with pytest.raises(TypeError) as type_error:
+        Dish("Teste", "5")
+    with pytest.raises(ValueError) as value_error:
+        Dish("Teste", -1)
+    ingredient_one = Ingredient("farinha")
+    dish_one = Dish("macarronada", 7.90)
+    dish_two = Dish("macarronada", 7.90)
+    dish_one.add_ingredient_dependency(ingredient_one, 1)
+    assert dish_one.get_restrictions() == {Restriction.GLUTEN}
+    assert dish_one.get_ingredients() == {ingredient_one}
+    assert dish_one == dish_two
+    assert dish_one.name == "macarronada"
+    assert hash(dish_one) == hash("Dish('macarronada', R$7.90)")
+    assert str(type_error.value) == "Dish price must be float."
+    assert str(value_error.value) == "Dish price must be greater then zero."
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,12 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ingredient_one = Ingredient("Tomate")
-    ingredient_two = Ingredient("Tomate")
-    assert ingredient_one.name == "Tomate"
-    assert hash(ingredient_one) == hash("Tomate")
-    assert str(ingredient_one) == "Ingredient('Tomate')"
+    ingredient_one = Ingredient("farinha")
+    ingredient_two = Ingredient("farinha")
+    assert ingredient_one.name == "farinha"
+    assert hash(ingredient_one) == hash("farinha")
+    assert str(ingredient_one) == "Ingredient('farinha')"
     assert ingredient_one == ingredient_two
-    assert ingredient_one.restrictions == set()
+    assert ingredient_one.restrictions == {Restriction.GLUTEN}
