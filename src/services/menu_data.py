539222a5b27c50d72a3041932620ff911952894e
Felipe Pinto <felipe14062000@gmail.com>
2023-06-17 16:05:34 -0300

    feat: implementação da função MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
+from csv import DictReader
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path) as file:
+            data = list(DictReader(file, delimiter=",", quotechar='"'))
+        dishes = {}
+        for item_menu in data:
+            name = item_menu["dish"]
+            price = float(item_menu["price"])
+            amount = int(item_menu["recipe_amount"])
+            ingredient = item_menu["ingredient"]
+            if name not in dishes:
+                dishes[name] = Dish(name, price)
+                dishes[name].add_ingredient_dependency(
+                    Ingredient(ingredient), amount
+                )
+            else:
+                dishes[name].add_ingredient_dependency(
+                    Ingredient(ingredient), amount
+                )
+        self.dishes = set(dishes.values())
