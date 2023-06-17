30066ae07b54dd1403e96cdefed4b1bbf6ebc51e
Felipe Pinto <felipe14062000@gmail.com>
2023-06-17 13:59:53 -0300

    test: implementando os testes da classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,7 +1,12 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# ...
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_one = Ingredient("Tomate")
+    ingredient_two = Ingredient("Tomate")
+    assert ingredient_one.name == "Tomate"
+    assert hash(ingredient_one) == hash("Tomate")
+    assert str(ingredient_one) == "Ingredient('Tomate')"
+    assert ingredient_one == ingredient_two
+    assert ingredient_one.restrictions == set()
